fc4c06294144d3c01f20cf10aa5aaf198dd75321
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-03-29 06:52:25 +0000

    remove superfluous doc

--- lib/mai_web.ex
+++ lib/mai_web.ex
@@ -1,21 +1,5 @@
 defmodule MaiWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, components, channels, and so on.
-
-  This can be used in your application as:
-
-      use MaiWeb, :controller
-      use MaiWeb, :html
-
-  The definitions below will be executed for every controller,
-  component, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define additional modules and import
-  those modules here.
-  """
+  @moduledoc false
 
   def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
@@ -89,9 +73,6 @@ defmodule MaiWeb do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
