b6addfb513f62f909c82f315b59f377201c6571e
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-04-16 09:27:16 +0000

    refactor

--- lib/mai_web/ui_state/index.ex
+++ lib/mai_web/ui_state/index.ex
@@ -3,21 +3,21 @@ defmodule MaiWeb.UiState.Index do
   alias Mai.Contexts.User
 
   def get(user_id) do
-    user = User.get_by_id(user_id)
-    periods = Chat.list_chats_by_period(user_id)
-
-    suggestions = Mai.Contexts.Suggestion.get_default()
-    input_value = Enum.random(suggestions).title
-
-    %{
-      sidebar: %{periods: periods, user: user},
-      main: %{suggestions: suggestions, input_value: input_value}
-    }
+    %{sidebar: get_sidebar(user_id), main: get_suggestions()}
   end
 
   def get(user_id, chat_id) do
     chat = Mai.Contexts.Chat.get_chat_details(chat_id)
-    index = get(user_id)
-    %{index | main: Map.put(index.main, :chat, chat)}
+    %{sidebar: get_sidebar(user_id), main: %{chat: chat}}
+  end
+
+  defp get_sidebar(user_id) do
+    %{periods: Chat.list_chats_by_period(user_id), user: User.get_by_id(user_id)}
+  end
+
+  defp get_suggestions() do
+    suggestions = Mai.Contexts.Suggestion.get_default()
+    input_value = Enum.random(suggestions).title
+    %{suggestions: suggestions, input_value: input_value}
   end
 end
