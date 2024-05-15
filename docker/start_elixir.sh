b24c9ae4b357a5e184eabb313b35a664fd75e51c
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-05-15 09:42:18 +0000

    both compose and supervisord

---
+++ docker/start_elixir.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait until PostgreSQL is ready
+until pg_isready -h $POSTGRES_HOST -p $PGPORT -U $POSTGRES_USER; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 2
+done
+
+echo "PostgreSQL is ready"
+
+# Run migrations
+/app/bin/mai eval "Mai.Release.migrate"
+
+# Start the Elixir app
+/app/bin/server
