49fa9f8d60a28cc769efad2589c6476cdff97f4e
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-05-23 13:42:57 +0000

    different permissions for migrations

--- dc-web-infra/run_migrations.sh
+++ dc-web-infra/run_migrations.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export POSTGRES_USER=${MIGRATION_USER}
+export POSTGRES_PASSWORD=${MIGRATION_PASSWORD}
+
 until pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER; do
   echo "Waiting for PostgreSQL to be ready..."
   sleep 2
